10bb6582cd89c26653444c9ee9dce5de939f8a78
Neeraj1005 <neeraj.tangariya123@gmail.com>
2022-02-07 13:36:54 +0530

    wip

--- app/Console/Commands/FetchSiteJobs.php
+++ app/Console/Commands/FetchSiteJobs.php
@@ -2,10 +2,11 @@
 
 namespace App\Console\Commands;
 
-use App\Http\Traits\JobSiteTrait;
-use App\Models\Country;
 use App\Models\Scrap;
+use App\Models\Country;
+use Illuminate\Support\Arr;
 use Illuminate\Console\Command;
+use App\Http\Traits\JobSiteTrait;
 
 class FetchSiteJobs extends Command
 {
@@ -50,40 +51,27 @@ class FetchSiteJobs extends Command
 
         $pages = $this->ask('How many pages do you want to scroll? [10, 20, 50 etc...]');
 
-        if ($siteName == 'jobbank') {
-            $countryId = $this->fetchCountry('canada');
-            $countryName = 'canada';
-        } elseif ($siteName == 'bayt') {
-            $countryName = $this->choice(
-                'select country',
-                ['uae', 'ind', 'canada'],
-                0,
-            );
-            $countryId = $this->fetchCountry($countryName);
-            $countryName = ($countryName == 'ind') ? 'india' : $countryName;
-        } else {
-            $countryName = $this->choice(
-                'select country',
-                ['uae', 'ind', 'canada'],
-                0,
-            );
-            $countryId = $this->fetchCountry($countryName);
-        }
+        $countryData = $this->get_selected_country_id_and_name($siteName)->first();
 
-        if ($this->confirm('Do you wish to continue? [site: ' . $siteName . ', country: '.$countryName. ' and pages: ' . $pages . ']', true)) {
+        if ($this->confirm(
+            '
+            Do you wish to continue? [site: ' . $siteName . ', country: ' . $countryData['country_name'] . ' and pages: ' . $pages . ']
+            ',
+            true
+        )) {
             $bar = $this->output->createProgressBar($pages);
 
             $bar->start();
 
             switch ($siteName) {
                 case 'bayt':
-                    $this->baytJobs($bar, $pages, $countryId, $countryName);
+                    $this->baytJobs($bar, $pages, $countryData['country_id'], $countryData['country_name']);
                     break;
                 case 'linkedin':
-                    $this->linkedInJobs($bar, $pages, $countryId, $countryName);
+                    $this->linkedInJobs($bar, $pages, $countryData['country_id'], $countryData['country_name']);
                     break;
                 case 'jobbank':
-                    $this->jobbankJobs($bar, $pages, $countryId, $countryName);
+                    $this->jobbankJobs($bar, $pages, $countryData['country_id'], $countryData['country_name']);
                     break;
             }
 
@@ -95,7 +83,44 @@ class FetchSiteJobs extends Command
         }
     }
 
-    protected function fetchCountry(string $country_name): int
+    /**
+     * based on selected sitename get country data
+     *
+     * @param string $siteName
+     */
+    protected function get_selected_country_id_and_name($siteName)
+    {
+        $arrayData = collect();
+
+        if ($siteName == 'jobbank') {
+            $countryId = $this->fetchCountryData('canada');
+            $countryName = 'canada';
+        } elseif ($siteName == 'bayt') {
+            $countryName = $this->choice(
+                'select country',
+                ['uae', 'ind', 'canada', 'usa', 'uk'],
+                0,
+            );
+            $countryId = $this->fetchCountryData($countryName);
+            $countryName = ($countryName == 'ind') ? 'india' : $countryName;
+        } else {
+            $countryName = $this->choice(
+                'select country',
+                ['uae', 'ind', 'canada', 'usa', 'uk'],
+                0,
+            );
+            $countryId = $this->fetchCountryData($countryName);
+        }
+
+        $data = $arrayData->push([
+            'country_id' => $countryId,
+            'country_name' => $countryName,
+        ]);
+
+        return $data;
+    }
+
+    protected function fetchCountryData(string $country_name): int
     {
         $country = Country::where('sortname', $country_name)->first();
 
